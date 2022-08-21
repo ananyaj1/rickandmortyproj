a3e6e48809d126342537234d29d4a3a1bba2a177
ananyaj1 <ananyaj@umich.edu>
2022-08-21 16:39:04 -0400

    API call works and displaying all ricks

--- src/App.js
+++ src/App.js
@@ -5,58 +5,69 @@ import './App.css';
 import { Grid } from '@mui/material';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect, useState } from "react";
 
-class Prediction extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: 'hey',
+
+function Prediction() {
+  var [ricks, setRicks] = useState([]);
+  useEffect(() => {
+    async function gettingRicks() {
+      const response = await fetch('https://rickandmortyapi.com/api/character/?name=rick');
+      const data = await response.json();
+      setRicks(data);
     }
-  }
-  selectCharacter = () => {
-    this.setState({selected: 'Rick'});
-  }
-  render() {
-    return (
-      <div className='App'>
-        <Grid container spacing={2}>
-          <Grid className='log' item xs={12}>
-            <img src={logo} />
-          </Grid>
-          <Grid item xs={12}>
-            <h1 className='text'>Rick's Crystal Ball</h1>
-          </Grid>
-          <Grid className='fortune App-logo' item xs={4}><img src={sparkle} align='right' /></Grid>
-          <Grid className='fortune ball' item xs={4}> <img src={ball} /></Grid>
-          <Grid className='fortune App-logo' item xs={4}> <img src={sparkle} align='left' /> </Grid>
-          <Grid item xs={4}></Grid>
-          <Grid item xs={4}>
-            <p className='text'>You have stumbled upon Rick's crystal ball! Choose one of the characters below,
-              and then click the crystal ball to reveal that character's location in the multiverse currently. Use this
-              information wisely! </p>
-          </Grid>
-          <Grid item xs={4}></Grid>
-          <Grid item className='char' xs={3}>{
-            //this.state.data.map((info, index)=> <Card key={index} info={info} />)
-            <Card/>
-          }</Grid>
-          <Grid><p>{this.state.checked}</p></Grid>
+    gettingRicks();
+  });
+  return (
+    <div className='App'>
+      <Grid container spacing={2}>
+        <Grid className='log' item xs={12}>
+          <img src={logo} />
         </Grid>
-      </div>
-    );
-  }
+        <Grid item xs={12}>
+          <h1 className='text'>Rick's Crystal Ball</h1>
+        </Grid>
+        <Grid className='fortune App-logo' item xs={4}><img src={sparkle} align='right' /></Grid>
+        <Grid className='fortune ball' item xs={4}> <img src={ball} /></Grid>
+        <Grid className='fortune App-logo' item xs={4}> <img src={sparkle} align='left' /> </Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <p className='text'>You have stumbled upon Rick's crystal ball! Choose one of the characters below,
+            and then click the crystal ball to reveal that character's location in the multiverse currently. Use this
+            information wisely! </p>
+        </Grid>
+        <Grid item xs={4}></Grid>
+          {ricks.results?.map(user => (
+            <Grid item xs={3} key={user.id}>
+              <div className='card'>
+              <div className='card-body'>
+                <img className='card-img-top' src={user.image}/>
+              <h5 className="card-title">{user.name}</h5>
+              <p className='card-body'>Species: {user.species}</p>
+              </div>
+            </div>
+            </Grid>
+            ))
+          //ricks.map(user => <Card key={user.id} title={user.name} />)
+         // this.state.data.map((info, index) => <Card key={index} info={info} />) 
+        }
+
+        <Grid><p></p></Grid>
+      </Grid>
+    </div>
+  );
 }
 
-const Card = (props) =>
-<button type="button" className="btn btn-light" onClick={this.selectCharacter}>
-<div className='holder'>
-<div className='card'>
-    <div className='card-body'>
-      <h5 className="card-title">Rick</h5>
-      <h6 className="card-subtitle mb-2 text-muted">character title</h6>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+const Card = (title) =>
+  <button type="button" className="btn btn-light">
+    <div className='holder'>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className="card-title">Rick</h5>
+          <h6 className="card-subtitle mb-2 text-muted">character title</h6>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</button>;
+  </button>;
 export default Prediction;
