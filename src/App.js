e4b960c031310889764750b18f70927f1ece0855
ananyaj1 <ananyaj@umich.edu>
2022-08-21 17:22:05 -0400

    location functionality works

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 
 function Prediction() {
   var [ricks, setRicks] = useState([]);
+  var [location, setLocation] = useState([]);
   useEffect(() => {
     async function gettingRicks() {
       const response = await fetch('https://rickandmortyapi.com/api/character/?name=rick');
@@ -18,6 +19,14 @@ function Prediction() {
     }
     gettingRicks();
   });
+    async function rickLocation(id) {
+      var url = 'https://rickandmortyapi.com/api/location/' + id;
+      const response = await fetch(url);
+      const known = await response.json();
+      setLocation(known);
+      console.log(known);
+    }
+
   return (
     <div className='App'>
       <Grid container spacing={2}>
@@ -32,24 +41,34 @@ function Prediction() {
         <Grid className='fortune App-logo' item xs={4}> <img src={sparkle} align='left' /> </Grid>
         <Grid item xs={4}></Grid>
         <Grid item xs={4}>
-          <p className='text'>You have stumbled upon Rick's crystal ball! Choose one of the characters below,
-            and then click the crystal ball to reveal that character's location in the multiverse currently. Use this
-            information wisely! </p>
+        {location ? (
+        <div className='loc'>
+          <p>{location.name}</p>
+          <p>{location.dimension}</p>
+          </div>) : null }
         </Grid>
         <Grid item xs={4}></Grid>
-          {ricks.results?.map(user => (
-            <Grid item xs={3} key={user.id}>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <p className='text'>You have stumbled upon Rick's crystal ball! This magical tool is how Rick keeps track of every Rick in the multiverse. Select
+            a Rick from below, and the crystal ball will reveal that Rick's last known location! </p>
+        </Grid>
+        <Grid item xs={4}></Grid> 
+        {ricks.results?.map(user => (
+          <Grid item xs={3} key={user.id}>
+            <button type="button" className="btn btn-dark" onClick={() => rickLocation(user.id)}>
               <div className='card'>
-              <div className='card-body'>
-                <img className='card-img-top' src={user.image}/>
-              <h5 className="card-title">{user.name}</h5>
-              <p className='card-body'>Species: {user.species}</p>
+                <div className='card-body'>
+                  <img className='card-img-top' src={user.image} />
+                  <h5 className="card-title">{user.name}</h5>
+                  <p className='card-body'>Species: {user.species}</p>
+                </div>
               </div>
-            </div>
-            </Grid>
-            ))
+            </button>
+          </Grid>
+        ))
           //ricks.map(user => <Card key={user.id} title={user.name} />)
-         // this.state.data.map((info, index) => <Card key={index} info={info} />) 
+          // this.state.data.map((info, index) => <Card key={index} info={info} />) 
         }
 
         <Grid><p></p></Grid>
@@ -57,8 +76,8 @@ function Prediction() {
     </div>
   );
 }
-
-const Card = (title) =>
+// testing code
+/* const Card = (title) =>
   <button type="button" className="btn btn-light">
     <div className='holder'>
       <div className='card'>
@@ -69,5 +88,6 @@ const Card = (title) =>
         </div>
       </div>
     </div>
-  </button>;
+  </button>;*/
+
 export default Prediction;
